80fed3f20f180b883531958a28948790438effb1
Mubarak Aliyu <mubeegraphics@gmail.com>
2026-03-23 17:15:21 +0100

    feat: add SEO meta tags and favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import React from "react";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </React.StrictMode>
+);
